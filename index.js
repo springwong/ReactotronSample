ec8c83ca0c015b1cbfcb86ba5e151155d870c2d8
Spring <development.spring@gmail.com>
2021-05-14 23:54:06 +0800

    connect Reactotron in index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+if (__DEV__) {
+    import("./ReactotronConfig")
+}
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
