8d098555539c9914c6deeb6e5628d2668ced1f6c
Spring <development.spring@gmail.com>
2021-05-15 00:12:03 +0800

    add argument support custom command

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,20 @@
 import Reactotron from 'reactotron-react-native'
+import { ArgType } from "reactotron-core-client";
 
 Reactotron
     .configure() // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .connect() // let's connect!
+
+Reactotron.onCustomCommand("Console Log", () => Reactotron.log("This is an example"))
+
+Reactotron.onCustomCommand({
+    command: "Custom Console Log",
+    handler: (params) => Reactotron.log(`Log Message: ${params.message}`),
+    args: [
+        {
+            name: "message",
+            type: ArgType.String,
+        }
+    ]
+})
